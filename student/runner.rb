25e9ac1e9495c847343bc94fa1134a720a506b02
Diego <diego2000avelar@gmail.com>
2022-11-13 08:34:47 -0600

    feat: student class

---
+++ student/runner.rb
@@ -0,0 +1,7 @@
+require_relative "student"
+
+test_student = Student.new("Diego", "Arevalo")
+
+puts test_student.first_name
+puts test_student.last_name
+puts test_student.student_fullname
---
+++ student/student_spec.rb
@@ -0,0 +1,18 @@
+require_relative "student"
+RSpec.describe Student do 
+    before do
+        @student = Student.new("John", "Doe")
+    end 
+    it "should respond to #first_name" do
+        expect(@student).to respond_to(:first_name)  
+    end
+
+    it "should respond to #last_name" do
+        expect(@student).to respond_to(:last_name)
+    end
+
+    it "should respond to #full_name" do
+        expect(@student).to respond_to(:student_fullname)
+        
+    end
+end
